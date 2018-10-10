0db218a36372ee575192a4d2fbff816b55837de9
toon <toon.b@excelbangkok.com>
2018-10-10 09:51:32 +0700

    angular5-seed application

---
+++ angular5-seed/src/app/layout/layout.routes.ts
@@ -0,0 +1,11 @@
+import { Routes, RouterModule } from '@angular/router';
+import { LayoutComponent } from './layout.component';
+const routes: Routes = [
+  { path: '', component: LayoutComponent, children: [
+    { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+    { path: 'dashboard', loadChildren: '../dashboard/dashboard.module#DashboardModule' },
+    { path: 'another-page', loadChildren: '../another/another.module#AnotherModule' },
+  ]}
+];
+
+export const ROUTES = RouterModule.forChild(routes);
